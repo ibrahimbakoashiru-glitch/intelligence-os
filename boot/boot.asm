f1646dc0a783b1a357c37e7e798c18a9747bd74b
Ibrahim Ashiru <ibrahimbakoashiru@gmail.com>
2026-02-18 13:22:55 +0100

    Initialize boot assembly with stack setup and kernel call

--- boot/boot.asm
+++ boot/boot.asm
@@ -1,1 +1,15 @@
+BITS 32
+GLOBAL _start
 
+_start:
+    cli                     ; disable interrupts
+    mov esp, stack_top      ; setup stack
+    call kernel_main        ; jump to kernel
+
+.hang:
+    hlt                     ; halt CPU
+    jmp .hang
+
+SECTION .bss
+resb 8192
+stack_top:
